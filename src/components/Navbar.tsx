a1b67c3e44a588dfa45c8ecec5e5debd7af954dd
TienDung <dungnt.ptitt@gmail.com>
2025-10-23 08:40:54 +0700

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,19 +4,19 @@ export default function Navbar() {
   const navigate = useNavigate();
 
   return (
-    <nav style={{ display: "flex", justifyContent: "space-between", padding: "10px 20px", background: "#222", color: "#fff" }}>
-      <h2 style={{ cursor: "pointer" }} onClick={() => navigate("/")}>📝 Blog Management</h2>
+    <nav style={{ display: "flex", justifyContent: "space-between", padding: "10px 30px", background: "#222", color: "#fff" }}>
+      <h2 style={{ cursor: "pointer" }} onClick={() => navigate("/")}> Blog Management</h2>
       <div>
         <NavLink
           to="/"
           style={({ isActive }) => ({
-            marginRight: "15px",
-            color: isActive ? "yellow" : "white",
+            marginRight: "20px",
+            color: isActive ? "Green" : "white",
           })}
         >
           Trang chủ
         </NavLink>
-        <button onClick={() => navigate("/create")}>✍️ Viết bài</button>
+        <button onClick={() => navigate("/create")}> Viết bài</button>
       </div>
     </nav>
   );
